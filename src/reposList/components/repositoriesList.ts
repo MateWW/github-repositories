ff271cf874e37bd36957bf3143b6f716083a24f5
Mateusz Wit <mateusz.wit@skygate.io>
2018-11-18 23:57:26 +0100

    Add styles

--- src/reposList/components/repositoriesList.ts
+++ src/reposList/components/repositoriesList.ts
@@ -4,8 +4,10 @@ import { GithubRepository } from '../model/GithubRepository';
 import { repositoriesListRow } from './repositoriesListRow';
 
 export const repositoriesList = (data: GithubRepository[]) => {
-    return h('table', { class: 'repositories-table' }, [
-        repositoriesListRow(null, true),
-        ...data.map(repository => repositoriesListRow(repository)),
+    return h('div', { key: 'keys', class: 'repositories-table-box' }, [
+        h('table', { class: 'repositories-table' }, [
+            repositoriesListRow(null, true),
+            ...data.map(repository => repositoriesListRow(repository)),
+        ]),
     ]);
 };
--- src/reposList/components/repositoriesListRow.ts
+++ src/reposList/components/repositoriesListRow.ts
@@ -3,18 +3,24 @@ import { h } from 'maquette';
 import { formatDate } from '../helpers/formatDate';
 import { emptyRepository, GithubRepository } from '../model/GithubRepository';
 
-const getCorrectListElement = (headerRow: boolean, value: string | number = null, headerName: string = null) => {
+const getCorrectListElement = (
+    headerRow: boolean,
+    value: string | number = null,
+    headerName: string = null,
+    keepTogether: boolean = false,
+) => {
     const tag = headerRow ? 'th' : 'td';
-    const className = `repositories-table-${headerRow ? 'header' : 'element'}`;
+    const className = `repositories-table-${headerRow ? 'header' : 'element'}${keepTogether ? ' keep-together' : ''}`;
     return h(tag, { class: className }, [headerRow ? headerName : `${value}`]);
 };
 
 export const repositoriesListRow = (repository: GithubRepository | null, headerRow = false) => {
     const { name, description, updated_at, downloads_url, id } = repository || emptyRepository;
-    return h('tr', { key: `${id}`, class: 'repositories-table-row' }, [
+    const className = `repositories-table-row${headerRow ? ' repositories-table-header-row' : ''}`;
+    return h('tr', { key: `key_${id}`, class: className }, [
         getCorrectListElement(headerRow, name, 'Name'),
         getCorrectListElement(headerRow, description || 'No description', 'Description'),
-        getCorrectListElement(headerRow, formatDate(updated_at), 'Last update'),
+        getCorrectListElement(headerRow, formatDate(updated_at), 'Last update', true),
         getCorrectListElement(headerRow, downloads_url, 'Download link'),
     ]);
 };
